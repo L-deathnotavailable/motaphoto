c7eaa1aec062a2195c9c5ab9da4db56b5c5a0b85
Ellie <elpassa339@gmail.com>
2024-02-24 11:05:05 +0100

    Js, css fontawsome, php
    Setting up the environment and menu

--- functions.php
+++ functions.php
@@ -1,10 +1,21 @@
 <?php
-// Chargement du style CSS et des scripts
+// Charging CSS styles and scripts
     function theme_enqueue_styles()
     {
         wp_enqueue_style('theme', get_template_directory_uri() . '/css/theme.css');
         wp_enqueue_style('style', get_template_directory_uri() . '/style.css');
+        // Library font awesome :
         wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css', array(), null);
     }
 
     add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+
+     // Add menu management to wordpress dashboard
+     function register_custom_menus() {
+        register_nav_menus(array(
+            'menu_principal' => __('Menu principal', 'Photographe'),
+            'menu_secondaire' => __('Menu secondaire', 'Photographe'),
+        ));
+    }
+     
+    add_action('init', 'register_custom_menus');
--- header.php
+++ header.php
@@ -1,37 +1,32 @@
-<?php
-/**
- * The header.
- *
- * This is the template that displays all of the <head> section and everything up until main.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package WordPress
- * @subpackage Twenty_Twenty_One
- * @since Twenty Twenty-One 1.0
- */
-
-?>
 <!doctype html>
-<html <?php language_attributes(); ?> <?php twentytwentyone_the_html_classes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<?php wp_head(); ?>
-</head>
+<html <?php language_attributes(); ?>>
 
-<body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content">
-		<?php
-		/* translators: Hidden accessibility text. */
-		esc_html_e( 'Skip to content', 'twentytwentyone' );
-		?>
-	</a>
+    <!-- Elements repris du theme hello elementor -->
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Photographe</title>
+        <?php wp_head(); ?>
+    </head>
 
-	<?php get_template_part( 'template-parts/header/site-header' ); ?>
+    <body>
+        <header>
+            <!-- Add a custom logo that can be modified via the dashboard-->
+            <div class="logo">
+                <?php the_custom_logo() ?>
+            </div>
+            
+            <nav>
+                <?php
+                wp_nav_menu(array(
+					// Calls up the main menu, which can be modified in the dashboard
+                    'theme_location' => 'menu_principal',
+                    'container' => false,
+                    'menu_class' => 'menu',
+                ));
+                ?>
+            </nav>
+						<!-- Other elements will be added later -->
+        </header>
 
-	<div id="content" class="site-content">
-		<div id="primary" class="content-area">
-			<main id="main" class="site-main">
+        <main>
