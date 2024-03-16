e446e1d56c71a5e28580d6b565d5a37af843cf0a
Ellie <elpassa339@gmail.com>
2024-03-16 11:32:22 +0100

    arrows and single page
    Add photos of arrows and create the single photo page with CSS

--- single-photo.php
+++ single-photo.php
@@ -18,8 +18,9 @@
         while ($custom_query->have_posts()) : $custom_query->the_post();
 
         $reference = get_field('reference');
-        $type = get_field('type');
-        $categories = wp_get_post_terms(get_the_ID(), 'categorie');
+        $type_de_photos = get_field('type_de_photos');
+        $annee = get_field('annee');
+        $categories = wp_get_post_terms(get_the_ID(), 'cate');
         $formats = wp_get_post_terms(get_the_ID(), 'formats');
 ?>
     <div class="articlephoto">
@@ -39,15 +40,117 @@
                     }
                     ?>
 				</p>
-				<p>TYPE : <?php echo $type;?></p>
-				<p>ANNÉE : <?php echo the_date('Y');?></p>
+				<p>TYPE : <?php echo $type_de_photos;?></p>
+				<p>ANNÉE : <?php echo $annee;?></p>
 			</div>
 		</div>
 		<!-- Zone droite - La photo -->
-		<div class="photo">
-			<div class="...">
+		<div class="photodroite">
+			<div class="PContent">
 				<?php the_content();?>
 			</div>
+		</div>
+	</div>
+
+    
+<!-- Ajout du bandeau d'interactions inférieur -->
+    <div class="...">
+		<div class="...">
+			<p>Cette photo vous intéresse ?</p>
+			<button class="...">Contact</button>
+		</div>
+		<div class="...">
+			<!-- Définition des bornes du tableau pour créer une boucle -->
+			<?php 
+				// Requête pour obtenir le dernier post
+				$args_dernier = array(
+					'post_type' => 'photographies', 
+					'posts_per_page' => 1,
+					'orderby' => 'date',
+					'order' => 'DESC',
+				);
+
+				$last_post = new WP_Query($args_dernier);
+
+				// Requête pour obtenir le premier post
+				$args_premier = array(
+					'post_type' => 'photographies', 
+					'posts_per_page' => 1,
+					'orderby' => 'date',
+					'order' => 'ASC',
+				);
+
+				$first_post = new WP_Query($args_premier);
+			?>
+			<div class="...">
+				<div class="...">
+					<!-- Récupération du post précédent par date de publication ASC (comportement par defaut) -->
+					<?php
+					$previous_post = get_previous_post();
+					// Si le post précédent existe, affichage du post précédent
+					if (!empty($previous_post)) :
+					?>
+						<a href="<?php echo get_permalink($previous_post); ?>">
+							<img class="..." src="<?php echo get_stylesheet_directory_uri() . '/assets/fleche-navigation-gauche.png' ?>" alt="Flèche de gauche" />
+						</a>
+					<!-- Si post précédent non-existant, affichage du dernier post publié -->
+					<?php else :
+						$last_post = $last_post->posts[0];
+					?>
+						<a href="<?php echo get_permalink($last_post); ?>">
+							<img class="..." src="<?php echo get_stylesheet_directory_uri() . '/assets/fleche-navigation-gauche.png' ?>" alt="Flèche de gauche" />
+						</a>
+					<?php endif; ?>
+				</div>
+				<div class="...">
+					<!-- Récupération du post suivant par date de publication ASC (comportement naturel) -->
+					<?php
+					$next_post = get_next_post();
+					// Si post suivant existant, affichage du post suivant
+					if (!empty($next_post)) :
+					?>
+						<a href="<?php echo get_permalink($next_post); ?>">
+							<img class="..." src="<?php echo get_stylesheet_directory_uri() . '/assets/fleche-navigation-droite.png' ?>" alt="Flèche de droite" />
+						</a>
+					<!-- Si post suivant non-existant, affichage du premier post publié -->
+					<?php else :
+						$first_post = $first_post->posts[0]; 
+					?>
+						<a href="<?php echo get_permalink($first_post); ?>">
+							<img class="..." src="<?php echo get_stylesheet_directory_uri() . '/assets/fleche-navigation-droite.png' ?>" alt="Flèche de droite"/>
+						</a>
+					<?php endif; ?>
+				</div>
+			</div>
+
+
+		<div class="div-vignettes">
+				<div class="conteneur-vignette-precedent">
+					<?php
+						// Récupération de la photo du post précédent
+						if (!empty($previous_post)) {
+							$miniature = get_post_field('post_content', $previous_post->ID);
+						} else {
+							$miniature = get_post_field('post_content', $dernier_post);
+						}
+						// Affichage du contenu
+						echo $miniature;
+					?>
+				</div>
+				<div class="conteneur-vignette-suivant">
+					<?php
+						// Récupération de la photo du post suivant
+						if (!empty($next_post)) {
+							$vignette = get_post_field('post_content', $next_post->ID);
+						} else {
+							$vignette = get_post_field('post_content', $first_post);
+						}
+						// Affichage du contenu
+						echo $vignette;
+					?>
+				</div>
+
+		  </div>
 		</div>
 	</div>
 
